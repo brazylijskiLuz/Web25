63334c30d80e861775fb9ccf246e5babb0b7a185
Kamil <moskalakamil07@gmail.com>
2025-10-04 18:40:11 +0200

    feat: customizable circle with avatar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,10 @@
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import { AvatarBackground } from "../../layout/avatar-background";
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Button>test</Button>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-      </footer>
-    </div>
+    <AvatarBackground circlePosition="right" assistant="pointing-left">
+      <Button>test</Button>
+    </AvatarBackground>
   );
 }
